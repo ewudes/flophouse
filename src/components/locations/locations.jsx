679b5de504e6d08b266e07ac96b23eca5c52dee6
ewudes <a.s.gunchin@gmail.com>
2021-03-12 15:21:43 +0400

    adds the locations component

---
+++ src/components/locations/locations.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {CITIES} from '../../const';
+import {connect} from 'react-redux';
+import {ActionCreator} from '../../store/action';
+import {PropTypes} from 'prop-types';
+
+const Locations = ({changeCity, city}) => {
+  const handleClickTabs = (currentCity) => {
+    changeCity(currentCity);
+  };
+
+  return (
+    <section className="locations container">
+      <ul className="locations__list tabs__list" >
+        {CITIES.map((item) => <li key={item} className="locations__item">
+          <a className={`locations__item-link tabs__item ${item === city ? `tabs__item--active` : ``}`} href="#">
+            <span onClick={() => handleClickTabs(item)}>{item}</span>
+          </a>
+        </li>)}
+      </ul>
+    </section>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  changeCity(city) {
+    dispatch(ActionCreator.changeCity(city));
+  }
+});
+
+const mapStateToProps = ({city}) => ({
+  city
+});
+
+Locations.propTypes = {
+  changeCity: PropTypes.func.isRequired,
+  city: PropTypes.string.isRequired
+};
+
+export {Locations};
+export default connect(mapStateToProps, mapDispatchToProps)(Locations);
