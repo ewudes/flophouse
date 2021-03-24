1bb3cdcb7b8e0d4a5b9a8dafcfe1c2c07150f572
ewudes <a.s.gunchin@gmail.com>
2021-03-24 05:14:37 +0400

    changes useRef to useState

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useState} from 'react';
 import {PropTypes} from 'prop-types';
 import Header from '../../components/header/header';
 import {login} from '../../store/api-actions';
@@ -6,24 +6,30 @@ import {connect} from 'react-redux';
 
 const Login = ({
   city,
-  userName,
   formSubmit
 }) => {
-  const loginRef = useRef();
-  const passwordRef = useRef();
+  const [credentials, setСredentials] = useState({
+    email: ``,
+    userPassword: ``
+  });
+
+  const handleFieldChange = (evt) => {
+    const {name, value} = evt.target;
+    setСredentials({...credentials, [name]: value});
+  };
 
   const formSubmitHandler = (evt) => {
     evt.preventDefault();
 
     formSubmit({
-      login: loginRef.current.value,
-      password: passwordRef.current.value,
+      login: credentials.email,
+      password: credentials.password,
     });
   };
 
   return (
     <div className="page page--gray page--login">
-      <Header userName={userName}/>
+      <Header />
       <main className="page__main page__main--login">
         <div className="page__login-container container">
           <section className="login">
@@ -31,11 +37,11 @@ const Login = ({
             <form className="login__form form" action="#" method="post" onSubmit={formSubmitHandler}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required="" ref={loginRef}/>
+                <input className="login__input form__input" type="email" name="email" placeholder="Email" required="" onChange={handleFieldChange}/>
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required="" ref={passwordRef}/>
+                <input className="login__input form__input" type="password" name="password" placeholder="Password" required="" onChange={handleFieldChange}/>
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
@@ -65,7 +71,6 @@ const mapDispatchToProps = (dispatch) => ({
 
 Login.propTypes = {
   city: PropTypes.string.isRequired,
-  userName: PropTypes.string,
   formSubmit: PropTypes.func.isRequired,
 };
 
