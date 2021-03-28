f03f46274d3d099f38741eb66e9a6159b7a51f5b
ewudes <a.s.gunchin@gmail.com>
2021-03-29 00:22:09 +0400

    fix the scroll position when changing the city

--- src/components/offer-list/offer-list.jsx
+++ src/components/offer-list/offer-list.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import Offer from '../offer/offer';
 import Map from '../map/map';
 import {offerProps} from '../prop-types/prop-types';
+import {useSelector} from 'react-redux';
 import Sort from '../sort/sort';
 
 const OfferList = ({
@@ -10,10 +11,19 @@ const OfferList = ({
   sortedOffers,
   city,
 }) => {
+  const {isDataLoaded} = useSelector((state) => state.DATA);
+
+  const citiesPlacesRef = useRef();
+
+  useEffect(() => {
+    if (isDataLoaded) {
+      citiesPlacesRef.current.scrollTop = 0;
+    }
+  }, [city]);
 
   return (
     <div className="cities__places-container container">
-      <section className="cities__places places">
+      <section className="cities__places places" ref={citiesPlacesRef}>
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">{currentOffers.length} places to stay in {city}</b>
         <Sort/>
