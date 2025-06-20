0b3dcfa6b8c09a83a7485d63addc4cacd59c9f04
Ewudes <ewudes@gmail.com>
2025-06-20 16:39:43 +0300

    fix: config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build')
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
     },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'build'),
-        open: false,
-        port: 1337,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
-        }, {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
-        }, {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: ['file-loader']
-        }
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devtool: 'source-map',
+    open: true,
+    port: 1337,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
 };
